b8f343b6efab82b3c2e98d724b3a3991f9ab6931
Trey Brittain <treybrittain@gmail.com>
2026-02-14 15:53:08 -0600

    chore: update application title to "Git Analytics"

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "git-analytics",
+		Title:  "Git Analytics",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
